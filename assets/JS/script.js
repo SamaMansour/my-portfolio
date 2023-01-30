ac46e082c2c7afc3f110ad407d621d72fca6b742
SamaMansour <sma302000@gmail.com>
2023-01-30 13:51:23 +0300

    Finish About me and add slideshow

---
+++ assets/JS/script.js
@@ -0,0 +1,47 @@
+var currentIndex = 0;
+var duration = 3000;
+var images = [
+    "../assets/Images/portal.png",
+    "../assets/Images/django.png"
+]
+// toggle to the correct section
+function toggleMenu() {
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+  }
+
+  // ReadMore Button
+  function loadMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+
+ // projects slideshow
+ slideShow();
+ 
+ function slideShow (){
+    let slide= document.getElementById('slide-img').src = images[currentIndex];
+
+    currentIndex++;
+
+    if (currentIndex == images.length){
+        currentIndex=0;
+    }
+    setTimeout(slideShow, duration);
+
+ }
+
+ 
