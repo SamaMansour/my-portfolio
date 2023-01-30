02423c59038c72be88d6b5e5924e0195a4d08c36
SamaMansour <sma302000@gmail.com>
2023-01-30 19:08:28 +0300

    Finish prortfolio with a need to refactor slideshow and navbar

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -4,43 +4,49 @@ var images = [
     "../assets/Images/portal.png",
     "../assets/Images/django.png"
 ]
+
+window.addEventListener("scroll", function () {
+	var nav = document.querySelector("nav");
+	nav.classList.toggle("sticky", window.scrollY > 0);
+  });
+  
 // toggle to the correct section
 function toggleMenu() {
-    var menuToggle = document.querySelector(".toggle");
-    var menu = document.querySelector(".menu");
-    menuToggle.classList.toggle("active");
-    menu.classList.toggle("active");
+	var menuToggle = document.querySelector(".toggle");
+	var menu = document.querySelector(".menu");
+	menuToggle.classList.toggle("active");
+	menu.classList.toggle("active");
   }
 
   // ReadMore Button
   function loadMore() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less";
-      moreText.style.display = "inline";
-    }
+		var dots = document.getElementById("dots");
+		var moreText = document.getElementById("more");
+		var btnText = document.getElementById("myBtn");
+
+		if (dots.style.display === "none") {
+			dots.style.display = "inline";
+			btnText.innerHTML = "Read more";
+			moreText.style.display = "none";
+		} else {
+			dots.style.display = "none";
+			btnText.innerHTML = "Read less";
+			moreText.style.display = "inline";
+		}
   }
 
  // projects slideshow
  slideShow();
- 
+
  function slideShow (){
-    let slide= document.getElementById('slide-img').src = images[currentIndex];
+	let slide= document.getElementById('slide-img').src = images[currentIndex];
 
-    currentIndex++;
+	currentIndex++;
 
-    if (currentIndex == images.length){
-        currentIndex=0;
-    }
-    setTimeout(slideShow, duration);
+	if (currentIndex == images.length){
+			currentIndex=0;
+	}
+	setTimeout(slideShow, duration);
 
  }
 
