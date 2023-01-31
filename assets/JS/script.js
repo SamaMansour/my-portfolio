d634a077e32f35f4c061e003a95ed4090d950f05
SamaMansour <sma302000@gmail.com>
2023-01-31 11:30:43 +0300

    Fix spaces and clean the code

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,16 +1,19 @@
 var currentIndex = 1;
-  var duration = 3000;
-  var images = [
-  "./assets/Images/portal.png",
-  "./assets/Images/portfolio.png",
-  "./assets/Images/foodi.png"
-  ]
-  var imageCount = 
-  [
-	  1,
-	  2,
-	  3
-  ]
+var duration = 3000;
+
+var images = [
+"./assets/Images/portal.png",
+"./assets/Images/portfolio.png",
+"./assets/Images/foodi.png"
+]
+
+var imageCount = 
+[
+	"1/3",
+	"2/3",
+	"3/3"
+]
+
 window.addEventListener("scroll", function () {
 	var nav = document.querySelector("nav");
 	nav.classList.toggle("sticky", window.scrollY > 0);
@@ -22,7 +25,7 @@ function toggleMenu() {
 	var menu = document.querySelector(".menu");
 	menuToggle.classList.toggle("active");
 	menu.classList.toggle("active");
-  }
+}
 
   // ReadMore Button
   function loadMore() {
@@ -47,15 +50,15 @@ function plusSlides(n) {
 		slideShow(currentIndex += n);
 	}
 }
-  function slideShow (){
-  let slide= document.getElementById('slide-img').src = images[currentIndex];
-	  document.getElementById('img-count').innerHTML = imageCount[currentIndex];
-  currentIndex++;
+function slideShow (){
+	let slide= document.getElementById('slide-img').src = images[currentIndex];
+	document.getElementById('img-count').innerHTML = imageCount[currentIndex];
+	currentIndex++;
 
-  if (currentIndex == images.length){
-	  currentIndex=0;
-  }
-  setTimeout(slideShow, duration);
+	if (currentIndex == images.length){
+		currentIndex=0;
+	}
+	setTimeout(slideShow, duration);
 
 }
 
