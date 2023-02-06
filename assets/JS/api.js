dc7936df6f38cdea7dd1bef1317b1e5011256d81
SamaMansour <sma302000@gmail.com>
2023-02-06 10:11:37 +0300

    Fix Links and add eslint

--- assets/JS/api.js
+++ assets/JS/api.js
@@ -1,29 +1,29 @@
 function createPost(){
-  let title = document.getElementById("title").value;
-  let body = document.getElementById("body").value;
-  fetch('http://localhost:3000/posts/', {
-  method: 'POST',
-  body: JSON.stringify({
-      title: title,
-      body: body,
-      userId: 1,
-  }),
-  headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-  },
-  })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+	let title = document.getElementById('title').value;
+	let body = document.getElementById('body').value;
+	fetch('http://localhost:3000/posts/', {
+		method: 'POST',
+		body: JSON.stringify({
+			title: title,
+			body: body,
+			userId: 1,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
 }
 
 document.addEventListener('DOMContentLoaded', function listPosts(){
-    fetch('http://localhost:3000/posts/')
-      .then((response) => response.json())
+	fetch('http://localhost:3000/posts/')
+		.then((response) => response.json())
   		.then((json) =>{
-						console.log(json);
-						var output = "";
-            json.forEach(function(post){
-							output += `
+			console.log(json);
+			var output = '';
+			json.forEach(function(post){
+				output += `
 							<div id="post" class= "shadow">
 								<h4 id="post-title">${post.title}</h4>
 								<p id="post-body">${post.body}</p>
@@ -32,57 +32,57 @@ document.addEventListener('DOMContentLoaded', function listPosts(){
 									<i class="fas fa-trash-alt" onclick="deletePost(${post.id})"></i>
 								</span>
 							</div>
-							`
+							`;
 
-							document.getElementById("posts").innerHTML = output;
+				document.getElementById('posts').innerHTML = output;
               
-            });
+			});
             
-        });
+		});
 			
 });
 
 
 function deletePost(id){
 	fetch(`http://localhost:3000/posts/${id}`, {
-  method: 'DELETE',
-});
+		method: 'DELETE',
+	});
 }
 
 function editPost(id){
-  var title1 = document.getElementById("title").value;
-  var body1 = document.getElementById("body").value;
-    fetch(`http://localhost:3000/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          id: id,
-          title: title1,
-          body: body1,
-          userId: 1,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+	fetch(`http://localhost:3000/posts/${id}`, {
+		method: 'PUT',
+		body: JSON.stringify({
+			id: id,
+			title: document.getElementById('title').value,
+			body: document.getElementById('body').value,
+			userId: 1,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	})
+		.then((response) => response.json())
+		.then((json) => console.log(json));
 }
 
 
 function scrollUp(id){
 
-  document.getElementById('left').scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
-  fetch(`http://localhost:3000/posts/${id}`)
-  .then((response) => response.json())
-  .then((json) => {console.log(json);
+	document.getElementById('left').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+	fetch(`http://localhost:3000/posts/${id}`)
+		.then((response) => response.json())
+		.then((json) => {console.log(json);
    
-      document.getElementById("title").value = json.title;
-      document.getElementById("body").value= json.body;
-      document.getElementById("edit-button").style.display = "block";
-      document.getElementById("edit-button").addEventListener("click", editPost(json.id));
-  }
+			document.getElementById('title').value = json.title;
+			document.getElementById('body').value= json.body;
+			document.getElementById('edit-button').style.display = 'block';
+      
 
-  );
+
+		}
+
+		);
   
 }
 
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,47 +2,47 @@ var currentIndex = 1 ;
 var duration = 3000;
 
 var images = [
-"./assets/Images/portal.png",
-"./assets/Images/portfolio.png",
-"./assets/Images/foodi.png"
-]
+	'./assets/Images/portal.png',
+	'./assets/Images/portfolio.png',
+	'./assets/Images/foodi.png'
+];
 
 var imageCount = 
 [
-	"1/3",
-	"2/3",
-	"3/3"
-]
+	'1/3',
+	'2/3',
+	'3/3'
+];
 
-window.addEventListener("scroll", function () {
-	var nav = document.querySelector("nav");
-	nav.classList.toggle("sticky", window.scrollY > 0);
-  });
+window.addEventListener('scroll', function () {
+	var nav = document.querySelector('nav');
+	nav.classList.toggle('sticky', window.scrollY > 0);
+});
   
 // toggle to the correct section
 function toggleMenu() {
-	var menuToggle = document.querySelector(".toggle");
-	var menu = document.querySelector(".menu");
-	menuToggle.classList.toggle("active");
-	menu.classList.toggle("active");
+	var menuToggle = document.querySelector('.toggle');
+	var menu = document.querySelector('.menu');
+	menuToggle.classList.toggle('active');
+	menu.classList.toggle('active');
 }
 
-  // ReadMore Button
-  function loadMore() {
-		var dots = document.getElementById("dots");
-		var moreText = document.getElementById("more");
-		var btnText = document.getElementById("myBtn");
+// ReadMore Button
+function loadMore() {
+	var dots = document.getElementById('dots');
+	var moreText = document.getElementById('more');
+	var btnText = document.getElementById('myBtn');
 
-		if (dots.style.display === "none") {
-			dots.style.display = "inline";
-			btnText.innerHTML = "Read more";
-			moreText.style.display = "none";
-		} else {
-			dots.style.display = "none";
-			btnText.innerHTML = "Read less";
-			moreText.style.display = "inline";
-		}
-  }
+	if (dots.style.display === 'none') {
+		dots.style.display = 'inline';
+		btnText.innerHTML = 'Read more';
+		moreText.style.display = 'none';
+	} else {
+		dots.style.display = 'none';
+		btnText.innerHTML = 'Read less';
+		moreText.style.display = 'inline';
+	}
+}
 
   
 function plusSlides(n) {
@@ -51,7 +51,7 @@ function plusSlides(n) {
 	}
 }
 function slideShow (){
-	let slide= document.getElementById('slide-img').src = images[currentIndex];
+	document.getElementById('slide-img').src = images[currentIndex];
 	document.getElementById('img-count').innerHTML = imageCount[currentIndex];
 	currentIndex++;
 
@@ -62,12 +62,12 @@ function slideShow (){
 
 }
 
-window.addEventListener("load", (event) => {
-  slideShow(); 
+window.addEventListener('load', (event) => {
+	slideShow(); 
 });
 
 function clearForm(){
-	var form = document.getElementById("contact-us");
+	var form = document.getElementById('contact-us');
 	form.reset();
 	
 
@@ -75,12 +75,12 @@ function clearForm(){
 
 
 function checkEmailDomain(){
-	let domain = ".*\.com$";
-	var email = document.getElementById("email").value;
+	let domain = '.*\.com$';
+	var email = document.getElementById('email').value;
 	if(!email.match(domain)){
-		alert("E-mail must end with .com");
+		alert('E-mail must end with .com');
 	}
-	window.location.href = "mailto:s.jamansour@gmail.com";
+	window.location.href = 'mailto:s.jamansour@gmail.com';
 	clearForm();
 	
 }
